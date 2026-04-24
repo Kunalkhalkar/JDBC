8d29962e6cedde7b86e644b203b51b47257a4386
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-04-24 20:37:11 +0530

    jdbc

--- com/practiceJDBCoperation/ResultSetPractice15.java
+++ com/practiceJDBCoperation/ResultSetPractice15.java
@@ -5,8 +5,13 @@ public class ResultSetPractice15 {
 	public static void main(String...x)throws Exception {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/onlineexam", "root", "root");
 		if(con != null) {
-			String query = "";
+			String query = "select * from students";
 			PreparedStatement psmt = con.prepareStatement(query);
+			ResultSet rs = psmt.executeQuery();
+			
+			while(rs.next()) {
+				System.out.printf("%d \t %s \t %s \n", rs.getInt(1), rs.getString(2), rs.getString(3));
+			}
 		}
 	}
 }
